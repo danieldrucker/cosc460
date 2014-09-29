5a15c4e51808671e2afdc827e8004483ec2f9242
danieldrucker <drucker.daniel@hotmail.com>
2014-09-29 14:30:42 -0400

    Somethings wrong

--- mysimpledb/src/java/simpledb/Catalog.java
+++ mysimpledb/src/java/simpledb/Catalog.java
@@ -150,7 +150,6 @@ public class Catalog {
         String baseFolder = new File(new File(catalogFile).getAbsolutePath()).getParent();
         try {
             BufferedReader br = new BufferedReader(new FileReader(new File(catalogFile)));
-
             while ((line = br.readLine()) != null) {
                 //assume line is of the format name (field type, field type, ...)
                 String name = line.substring(0, line.indexOf("(")).trim();
